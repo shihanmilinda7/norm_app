dc1c024a0a03980a579fc1a9136cb54353b9bed3
Milinda Ruberu <87467189+shihanmilinda7@users.noreply.github.com>
2024-05-12 13:14:04 +0530

    nn

--- src/components/items/norm/norm_actions.tsx
+++ src/components/items/norm/norm_actions.tsx
@@ -20,5 +20,5 @@ export const insertNorm = async (data: any) => {
   } else {
     return Promise.reject({ success: false, msg: "Insert failed" });
   }
-  //  // return info;cc
+  //  // return info;
 };
